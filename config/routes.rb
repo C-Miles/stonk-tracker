3ea62cbec0c5dac9c874e3308775ee56e18f33ab
Chris Miles <cmiles@obm.com>
2022-07-03 09:51:11 -0400

    setup front-end structure for stock lookup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
